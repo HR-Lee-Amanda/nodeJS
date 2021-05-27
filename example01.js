51f631b3ad9bced141a376d7da448df3b9663743
DESKTOP-OGF00CJ\Amanda <gpfla7946@gmail.com>
2021-05-28 00:23:35 +0900

    $ npm init : Create package.json
    exports.fun = function () {}
    module.exports = object;
    
    const calc = require('./filenameWithoutExtension');

--- example01.js
+++ example01.js
@@ -1,5 +1,7 @@
-const calc1 = require('./example02');
-const calc2 = require('./example03');
+const calc1 = require('./example02'); // 내가 만든 모듈
+const calc2 = require('./example03'); // 내가 만든 모듈
+const os = require('os'); // 내장 모듈 : 시스템 정보를 알려주는 모듈
+const path = require('path'); // 내장 모듈 : 파일의 경로를 다루는 모듈
 
 //console.log("new start");
 //console.log("Decimal Integer : %d", 11);
@@ -30,6 +32,32 @@ process.argv.forEach(function(item, index) {
 });
 
 console.log("************************************************");
+
 console.log(calc1.add(3,4));
 console.log(calc2.add(4,5));
+
+console.log("************************************************");
+/*
+console.log("시스템의 hostname : %s", os.hostname());
+console.log("시스템의 메모리 : %d / %d", os.freemem, os.totalmem());
+console.log("시스템의 CPU 정보");
+console.dir(os.cpus());
+console.log("시스템의 네트워크 인터페이스 정보");
+console.log(os.networkInterfaces());
+
+console.log("************************************************");
+*/
+
+var directories = ["users", "mike", "docs"];
+var docsDirectory = directories.join(path.sep);
+console.log("문서 디렉터리 : %s", docsDirectory);
+
+var curPath = path.join("/Users/mike", 'notepad.exe');
+console.log('파일 패스 : %s', curPath);
+var filename = "C:\\Users\\mike\\notepad.exe";
+var dirname = path.dirname(filename);
+var basename = path.basename(filename);
+var extname = path.extname(filename);
+console.log("디렉터리 : %s, 파일 이름 : %s, 확장자 : %s", dirname, basename, extname);
+
 console.log("************************************************");
